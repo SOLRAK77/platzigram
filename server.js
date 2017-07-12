289c078410e9b7f0b9fb82c2a41760166cc00bc4
SOLRAK77 <ccastillo.perez77@gmail.com>
2017-07-11 23:57:52 -0500

    Se agrego el primer js para el cliente.  - browserify, babel

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ var express = require('express')
 
 var app = express();
 
+app.set('view engine','pug');
 
-app.set('view engine','pug')
-app.use(express.static('css'))
+app.use(express.static('css'));
 
 app.get('/',function(req,res){
   res.render('index')
 })
 
+app.use(express.static(__dirname + '/public')); 
 app.listen(3000, function(err){
   if (err) return console.log('Huno un erro'), process.exit(0);
   console.log('platzigram escuchando en el puerto 3000')
---
+++ src/index.js
@@ -0,0 +1,7 @@
+var numeros = [400, 200, 1, -23];
+
+var numeroMas1 = numeros.map(function(numero){
+  return numero + 1
+})
+
+console.log(numeroMas1);
